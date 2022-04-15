754afed5081de3dc991edc53d513d047ff4876d2
qcraft <qcraft@BJ-jiangbowen.qcraft.lan>
2022-04-15 14:38:10 +0800

    arco-cli: initialize Qcraft-comp

---
+++ .config/jest.config.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+const { getGitRootPath } = require('arco-cli-dev-utils');
+
+module.exports = {
+  /**
+   * @param config {import('arco-scripts').JestConfig}
+   */
+  node: (config) => {
+    const rootConfigPath = path.resolve(getGitRootPath(), 'arco.scripts.config.js');
+    if (fs.existsSync(rootConfigPath)) {
+      const { jest } = require(rootConfigPath);
+      if (jest && jest.node) {
+        config = jest.node(config) || config;
+      }
+    }
+
+    config.testPathIgnorePatterns = ['/node_modules/', '/scripts/'];
+    config.moduleNameMapper = {
+      '^Qcraft-comp/(.+)$': '<rootDir>/$1',
+      '^Qcraft-comp$': '<rootDir>',
+    };
+  },
+  /**
+   * @param config {import('arco-scripts').JestConfig}
+   */
+  client: (config) => {
+    const rootConfigPath = path.resolve(getGitRootPath(), 'arco.scripts.config.js');
+    if (fs.existsSync(rootConfigPath)) {
+      const { jest } = require(rootConfigPath);
+      if (jest && jest.client) {
+        config = jest.client(config) || config;
+      }
+    }
+
+    config.testPathIgnorePatterns = ['/node_modules/', '/scripts/'];
+    config.collectCoverageFrom = ['components/**/*.{ts,tsx}', '!components/**/style/*'];
+    config.coveragePathIgnorePatterns = [
+      '/node_modules/',
+      '/lib/',
+      '/es/',
+      '/dist/',
+      '/components/index.tsx',
+      '/components/locale/',
+    ];
+    config.moduleNameMapper = {
+      '^Qcraft-comp/(.+)$': '<rootDir>/$1',
+      '^Qcraft-comp$': '<rootDir>',
+    };
+  },
+};
---
+++ scripts/template/component/demo/basic.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { @CONST_COMPONENT_NAME@ } from 'Qcraft-comp';
+
+export default () => {
+  return <@CONST_COMPONENT_NAME@ />;
+};
